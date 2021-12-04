b4c155d46805582fa52f8e966ecbd043610075dc
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-12-04 10:44:51 +0800

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**                                                  
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+
+public class Robot extends TimedRobot {
+  XboxController joy = new XboxController(0);
+  VictorSP suck = new VictorSP(5);
+  VictorSP shoot = new VictorSP(4);
+  VictorSP base1 = new VictorSP(0);
+  VictorSP base2 = new VictorSP(1);
+  VictorSP base3 = new VictorSP(2);
+  VictorSP base4 = new VictorSP(3);
+  Servo servo = new Servo(6);
+  VictorSP barrier = new VictorSP(7);
+  Timer timer = new Timer();
+
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    base1.set(-joy.getRawAxis(1)/3);
+    base2.set(joy.getRawAxis(5)/3);
+    base3.set(joy.getRawAxis(5)/3);
+    base4.set(-joy.getRawAxis(1)/3);
+    
+    if(joy.getRawButton(4)){
+      shoot.set(0.7);
+    }
+    else{
+      shoot.set(0);
+    }
+
+    if(joy.getRawButtonPressed(4)){
+      timer.start();
+    }
+    else if(joy.getRawButtonReleased(4)){
+      timer.stop();
+      timer.reset();  
+    }
+
+    if(timer.get()>0.6&&timer.get()<1.2){
+      barrier.set(0.3);
+    }
+    else if(timer.get()>1.2){
+      barrier.set(0);
+    }
+    else{
+      barrier.set(1);
+    }
+
+    if(joy.getRawButton(6)){
+      suck.set(-0.7);
+    }
+    else if(joy.getRawButton(5)){
+      suck.set(0.7);
+    }
+    else{
+      suck.set(0);;
+    }
+
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+}
